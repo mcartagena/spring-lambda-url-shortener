fbdf2296a65f00438ab9d529ee5d12523b9864e4
mcartagena <marcelocartagenaorellana@gmail.com>
2021-01-16 15:42:36 -0300

    Creating the project

---
+++ src/test/java/com/mcartagena/springlambdaurlshortener/SpringLambdaUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcartagena.springlambdaurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLambdaUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
